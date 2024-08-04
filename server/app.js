90ffcb9747cd7ff64fc568a3ada5bee550c4b068
san783 <sakthisanjay.vijayanand@my.utsa.edu>
2024-08-04 18:20:09 -0500

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 4000;
 
 app.use(cors())
+app.use(bodyParser.json());
+
 app.listen(port, () => {
     console.log('server listening on port ${port}')
 })
@@ -11,3 +14,9 @@ app.listen(port, () => {
 app.get('/', (req, res) => {
     res.send('Hello from our server!')
 })
+
+app.post('/session', (req, res) => {
+  const { duration } = req.body;
+  console.log(`Session duration: ${duration} seconds`);
+  res.send('Session saved!');
+});
