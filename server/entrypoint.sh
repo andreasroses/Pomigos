6371c4003a34ae5f510712b588a7397ab27eb85d
PeytonMalveauxTCXR <PeytonM.TCXR@gmail.com>
2024-08-07 22:00:44 -0500

    Entrypoint file made

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -6,7 +6,7 @@ PROJECT_ID="nifty-yeti-429817-d1"
 SECRET_ID="google-cloud-credentials"
 
 # Use gcloud to access the secret and write to a file
-gcloud secrets versions access latest --secret="${SECRET_ID}" > "${GOOGLE_APPLICATION_CREDENTIALS}"
+gcloud secrets versions access latest --secret="${SECRET_ID}" --project="${PROJECT_ID}" > "${GOOGLE_APPLICATION_CREDENTIALS}"
 
 # Execute the CMD
 exec "$@"
