a5f8c54dce152c0145a9b7e409757a1ee7d8ffe6
PeytonMalveauxTCXR <PeytonM.TCXR@gmail.com>
2024-08-07 20:18:52 -0500

    update route to json

--- server/app.py
+++ server/app.py
@@ -8,29 +8,34 @@ import string
 from google.cloud.sql.connector import Connector
 import os
 import time
+import logging
 
 pymysql.install_as_MySQLdb()
 
 app = Flask(__name__)
 socketio = SocketIO(app)
-#app = Flask(__name__, static_folder='static')
 
-# Set environment variable for Google Cloud credentials/
-# json file in trello
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "C:/Users/cdmal/OneDrive/Documents/1cloudsystems/nifty-yeti-429817-d1-5ab9ebaadbe3.json"
+# Set environment variable for Google Cloud credentials
+google_credentials_path = os.getenv('GOOGLE_APPLICATION_CREDENTIALS', './nifty-yeti-429817-d1-5ab9ebaadbe3.json')
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = google_credentials_path
 
 # Create a Cloud SQL Connector instance
 connector = Connector()
 
 def getconn():
-    conn = connector.connect(
-        "nifty-yeti-429817-d1:us-central1:pomigosdb",
-        "pymysql",
-        user="pomigos",
-        password="pomigos",
-        db="pomigosdb"
-    )
-    return conn
+    try:
+        conn = connector.connect(
+            "nifty-yeti-429817-d1:us-central1:pomigosdb",
+            "pymysql",
+            user="pomigos",
+            password="pomigos",
+            db="pomigosdb"
+        )
+        logging.info("Database connection established")
+        return conn
+    except Exception as e:
+        logging.error(f"Error connecting to the database: {e}")
+        raise
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql:///"
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
@@ -42,20 +47,19 @@ CORS(app)
 
 class Room(db.Model):
     room_id = db.Column(db.String(100), primary_key=True)
-# Task model
+
 class Task(db.Model):
     __tablename__ = 'task'
     task_id = db.Column(db.Integer, primary_key=True)
     task_name = db.Column(db.String(255), nullable=False)
     task_description = db.Column(db.String(255), nullable=False)
     completion = db.Column(db.Boolean, default=False)
-    board_id = db.Column(db.Integer, foreign_key=True)
+    board_id = db.Column(db.Integer, db.ForeignKey('board.board_id'), nullable=False)
 
-#Board model
 class Board(db.Model):
     board_id = db.Column(db.Integer, primary_key=True)
-    room_id = db.Column(db.String(100), primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    room_id = db.Column(db.String(100), db.ForeignKey('room.room_id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
     shared = db.Column(db.Boolean, default=False)
     board_name = db.Column(db.String(100), nullable=False)
 
@@ -63,7 +67,6 @@ class User(db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
 
 def after_insert_task(mapper, connection, target):
-    # Notify clients about the new task
     socketio.emit('task_added', {'task': {
         'task_id': target.task_id,
         'task_name': target.task_name,
@@ -72,7 +75,6 @@ def after_insert_task(mapper, connection, target):
     }})
 
 def after_insert_board(mapper, connection, target):
-    # Notify clients about the new board
     socketio.emit('board_added', {'board': {
         'board_id': target.board_id,
         'board_name': target.board_name,
@@ -81,19 +83,17 @@ def after_insert_board(mapper, connection, target):
     }})
 
 def after_update_board(mapper, connection, target):
-    # Notify clients about the new board
     socketio.emit('board_updated', {'board': {
         'board_id': target.board_id,
         'board_name': target.board_name,
         'user_id': target.user_id,
         'shared' : target.shared
     }})
-# Basic homepage route
+
 @app.route('/')
 def index():
     return send_from_directory(app.static_folder, 'index.html')
 
-# Route to fetch tasks
 @app.route('/tasks')
 def get_tasks():
     try:
@@ -101,21 +101,21 @@ def get_tasks():
         tasks_list = [{'task_id': task.task_id, 'task_name': task.task_name, 'task_description': task.task_description, 'completion': task.completion, 'board_id': task.board_id} for task in tasks]
         return jsonify(tasks_list)
     except Exception as e:
+        logging.error(f"Error fetching tasks: {e}")
         return jsonify({'error': str(e)})
 
-# Route to add a new task
 @app.route('/add_task', methods=['POST'])
 def add_task():
     try:
         data = request.get_json()
-        new_task = Task(task_name=data['task_name'], task_description=data['task_description'],board_id=data['board_id'])
+        new_task = Task(task_name=data['task_name'], task_description=data['task_description'], board_id=data['board_id'])
         db.session.add(new_task)
         db.session.commit()
         return jsonify({'task_id': new_task.task_id, 'task_name': new_task.task_name, 'task_description': new_task.task_description})
     except Exception as e:
+        logging.error(f"Error adding task: {e}")
         return jsonify({'error': str(e)})
 
-# Route to delete a task
 @app.route('/delete_task', methods=['POST'])
 def delete_task():
     try:
@@ -128,9 +128,9 @@ def delete_task():
             return jsonify({'message': 'Task deleted successfully'})
         return jsonify({'error': 'Task not found'})
     except Exception as e:
+        logging.error(f"Error deleting task: {e}")
         return jsonify({'error': str(e)})
 
-# Route to mark task as complete
 @app.route('/complete_task', methods=['POST'])
 def complete_task():
     try:
@@ -143,21 +143,20 @@ def complete_task():
             return jsonify({'message': 'Task marked as complete'})
         return jsonify({'error': 'Task not found'})
     except Exception as e:
+        logging.error(f"Error marking task as complete: {e}")
         return jsonify({'error': str(e)})
 
-@app.route('/add_board',methods=['POST'])
+@app.route('/add_board', methods=['POST'])
 def add_board():
     try:
         data = request.get_json()
-
         new_board = Board(
             user_id=data['user_id'],
             board_name=data['board_name'],
-            shared=data.get('shared', False)  # Default to False if not provided
+            shared=data.get('shared', False)
         )
         db.session.add(new_board)
         db.session.commit()
-        
         return jsonify({
             'board_id': new_board.board_id,
             'user_id': new_board.user_id,
@@ -165,9 +164,9 @@ def add_board():
             'shared': new_board.shared
         })
     except Exception as e:
+        logging.error(f"Error adding board: {e}")
         return jsonify({'error': str(e)})
 
-# Route to save Pomodoro session
 @app.route('/session', methods=['POST'])
 def save_session():
     try:
@@ -178,27 +177,23 @@ def save_session():
             'duration': duration,
             'timestamp': timestamp
         }
-        # Here you would typically save the session to a database
         return jsonify({'message': 'Session saved', 'session': session}), 201
     except Exception as e:
+        logging.error(f"Error saving session: {e}")
         return jsonify({'error': str(e)})
 
-@app.route('/update_board/<int:board_id>',methods=['PUT'])
+@app.route('/update_board/<int:board_id>', methods=['PUT'])
 def update_board(board_id):
     try:
         data = request.get_json()
         board = Board.query.get(board_id)
-        
         if not board:
             return jsonify({'error': 'Board not found'}), 404
-
         if 'board_name' in data:
             board.board_name = data['board_name']
         if 'shared' in data:
             board.shared = data['shared']
-
         db.session.commit()
-        
         return jsonify({
             'board_id': board.board_id,
             'user_id': board.user_id,
@@ -206,37 +201,34 @@ def update_board(board_id):
             'shared': board.shared
         })
     except Exception as e:
+        logging.error(f"Error updating board: {e}")
         return jsonify({'error': str(e)}), 400
 
 @app.route('/add_room', methods=['POST'])
 def add_room():
     try:
-        roomID = ''.join(secrets.choice(string.ascii_uppercase + string.digits)
-            for i in range(8))
+        roomID = ''.join(secrets.choice(string.ascii_uppercase + string.digits) for i in range(8))
         newRoom = Room(room_id=roomID)
         db.session.add(newRoom)
         db.session.commit()
         return jsonify({'room_id': newRoom.room_id})
     except Exception as e:
+        logging.error(f"Error adding room: {e}")
         return jsonify({'error': str(e)})
 
 @app.route('/join_room/<room_id>', methods=['GET'])
 def join_room(room_id):
-    # Fetch the room details and handle room joining logic
     room = Room.query.filter_by(room_id=room_id).first()
     if room:
         return jsonify({'room_id': room.room_id})
     else:
         return jsonify({'error': 'Room not found'}), 404
 
-event.listen(Board, 'after_insert', after_insert_board)
-event.listen(Board,'after_update',after_update_board)
-
 @app.route('/<path:path>')
 def static_proxy(path):
     return send_from_directory(app.static_folder, path)
 
 if __name__ == '__main__':
-    socketio.run(app)
-    #socketio.run(app, host='0.0.0.0', port=5000)
-    #app.run(debug=True)
+    app.config['DEBUG'] = True  # Enable debug mode
+    logging.basicConfig(level=logging.DEBUG)  # Set logging level to debug
+    socketio.run(app, debug=True)
